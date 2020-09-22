5267db91307029c724f6d2fade4768682f73a0ab
Patrick Rose <pjr0911025@googlemail.com>
2020-09-22 23:23:58 +0100

    Change runs to be on a per-facility basis

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 # bot.py
+import datetime
 import json
 import os
 
@@ -37,71 +38,94 @@ async def on_command_error(ctx, error):
 
 
 @bot.command(
-    name='create-run',
-    help='Create a run text+voice channel, limited to the run-* role - use mentions to assign roles automatically'
+    name='run-facility',
 )
-@commands.has_role('control')
-async def create_run(ctx):
-    guild = ctx.guild
-    run_number = 1
-    channel_name = f"run-{run_number}"
+async def create_run(ctx, short_corp: str, facility: str):
+    guild: discord.Guild = ctx.guild
 
-    # Work out what's the new run number
-    while discord.utils.get(guild.channels, name=channel_name):
-        run_number += 1
-        channel_name = f"run-{run_number}"
+    if short_corp not in CORPORATION_NAMES:
+        await ctx.send(
+            '{0} - {1} not found (must be one of {2})'.format(
+                ctx.message.author.mention,
+                short_corp,
+                ', '.join(CORPORATION_NAMES)
+            )
+        )
+        return
 
-    # Next, create the run-* role
-    role = discord.utils.get(guild.roles, name=channel_name)
-    control = discord.utils.get(guild.roles, name='control')
-    if not role:
-        role = await guild.create_role(name=channel_name)
-
-    category = discord.utils.get(guild.categories, name='runs')
+    category: discord.CategoryChannel = discord.utils.get(guild.categories, name='runs')
     if not category:
         category = await guild.create_category('runs')
 
-    text_channel = await guild.create_text_channel(
-        channel_name,
-        category=category,
-        overwrites={
-            guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            control: discord.PermissionOverwrite(read_messages=True),
-            role: discord.PermissionOverwrite(read_messages=True)
-        }
-    )
-    await guild.create_voice_channel(
-        channel_name,
-        category=category,
-        overwrites={
-            guild.default_role: discord.PermissionOverwrite(view_channel=False),
-            control: discord.PermissionOverwrite(view_channel=True),
-            role: discord.PermissionOverwrite(view_channel=True)
-        }
-    )
+    text_channel: discord.TextChannel = discord.utils.get(category.text_channels,
+                                                          name=f'{short_corp}-{facility.lower()}')
 
-    await ctx.send(f'Created text/voice channels {channel_name}')
-
-    channel_message = ''
-
-    # Now go through the list of people in the run and give them the run role
-    for user in ctx.message.mentions:
-        try:
-            await user.add_roles(role)
-            channel_message += f'{user.mention} '
-        except Exception as e:
-            await ctx.send(
-                f'{control.mention} Could\'t give {user.name} the role {role.name}! Please assign manually'
+    if not text_channel:
+        await ctx.send(
+            '{0} - {1} not found as a facility for {2}'.format(
+                ctx.message.author.mention,
+                facility,
+                CORPORATION_NAMES[short_corp]
             )
-            print(e)
+        )
+        return
 
-    if channel_message:
-        await text_channel.send(f'{channel_message.strip()}, please report to this channel to resolve the run')
+    # Make sure the runner isn't already on a run - that'd be naughty!
+    for role in ctx.author.roles:
+        if role.name.find('run-') == 0:
+            await ctx.send(
+                f'{ctx.message.author.mention} - you are already on a run!'
+            )
+            return
 
-    await text_channel.send("When resolving rolls, use the `!roll` command.\nTo roll 6 d8 and count the number of "
-                            "success, use `!roll 6d8 t5`.\nTo roll 6 d6 and count the number of successes, "
-                            "use `!roll 6d6 t5`\nYou can play cards using the `!play` command - see your "
-                            "relevant Google doc for the command you need")
+    voice_channel: discord.VoiceChannel = discord.utils.get(category.voice_channels,
+                                                            name=f'{short_corp}-{facility.lower()}')
+
+    role = None
+
+    # Check if there's a run role already - if not we'll create a new one
+    for key in text_channel.overwrites:
+        if isinstance(key, discord.Role) and key.name.find('run-') == 0:
+            role = key
+            break
+
+    send_initiation_message = False
+
+    if not role:
+        # Work out what's the new run number
+        while True:
+            import random
+
+            random_bytes = random.getrandbits(16)
+
+            print("hash value: %032x" % random_bytes)
+
+            role_name = f"run-{random_bytes}"
+            # Next, create the run-* role
+            role = discord.utils.get(guild.roles, name=role_name)
+
+            if not role:
+                await guild.create_role(name=role_name)
+                send_initiation_message = True
+                break
+
+    await text_channel.set_permissions(role, read_messages=True)
+    await voice_channel.set_permissions(role, view_channel=True)
+
+    corp_role: discord.Role = discord.utils.get(guild.roles, name=CORPORATION_ROLE_NAMES[short_corp])
+
+    if send_initiation_message:
+        await text_channel.send(
+            f"!!! RUN INITIATED !!!\n{corp_role.mention} please send your security representative to defend")
+
+        await text_channel.send("When resolving rolls, use the `!roll` command.\nTo roll 6 d8 and count the number of "
+                                "success, use `!roll 6d8 t5`.\nTo roll 6 d6 and count the number of successes, "
+                                "use `!roll 6d6 t5`\nYou can play cards using the `!play` command - see your "
+                                "relevant Google doc for the command you need")
+
+    await ctx.message.author.add_roles(role)
+
+    await text_channel.send(f'{ctx.message.author.mention} has joined the run')
 
 
 @bot.command(name='clear-runs', help='Deletes *all* run channels and roles for end of turn clean up')
@@ -111,23 +135,30 @@ async def clear_runs(ctx):
 
     # Get all channels in this category
     category = discord.utils.get(guild.categories, name='runs')
-    channels = category.channels
-    await ctx.send(f'Deleting {len(channels)} channels, please wait...')
+    channels = category.text_channels
+    await ctx.send(f'Deleting content in {len(channels)} channels, please wait...')
 
-    roles_done = []
-
-    for channel in channels:
-        role = discord.utils.get(guild.roles, name=channel.name)
-        if channel.name != "run-bot-commands":
-            await channel.delete()
-
-        if role and role not in roles_done:
+    from discord import Role
+    role: Role
+    for role in guild.roles:
+        if role.name.find('run-') == 0:
             await role.delete()
-            roles_done.append(role)
 
-    await guild.fetch_channels()
+    channel: discord.TextChannel
+    delta = datetime.timedelta(days=14)
+    for channel in channels:
+        msgs = await channel.history(limit=100, after=datetime.datetime.now() - delta).flatten()
+        if not msgs:
+            continue
+
+        try:
+            await ctx.send(f'Deleting content in {channel.name}')
+            await channel.delete_messages(msgs)
+        except discord.DiscordException:
+            continue
+
+    await ctx.send(f'Runs cleared')
     await guild.fetch_roles()
-    await ctx.send(f'Channels and roles deleted')
 
 
 @bot.command(name='play', help='Plays the card with the given name')
@@ -140,7 +171,13 @@ async def play_card(ctx: commands.context.Context, card: str):
         await ctx.send(f'{ctx.message.author.nick or ctx.message.author.name} plays {card_name}', file=file)
 
 
-async def create_category(guild, name, overwrites={}, text_channels={}, voice_channels={}):
+async def create_category(guild, name, overwrites=None, text_channels=None, voice_channels=None):
+    if voice_channels is None:
+        voice_channels = {}
+    if text_channels is None:
+        text_channels = {}
+    if overwrites is None:
+        overwrites = {}
     category = discord.utils.get(guild.categories, name=name)
 
     if not category:
@@ -179,31 +216,28 @@ CORPORATION_NAMES = {
     'dtc': 'Digital Tactical Control',
     'genetic': 'Genetic Equity',
     'gordon': 'Gordon Corporation',
-    'muccullough': 'MucCullough Mechanical'
+    'mccullough': 'McCullough Mechanical'
 }
 
-FACILITY_TYPES = [
-    'research',
-    'security',
-    'corporate',
-    'ai-school'
-]
+CORPORATION_ROLE_NAMES = {
+    'augmented': 'augmented-nucleotech',
+    'dtc': 'digital-tactical-control',
+    'genetic': 'genetic-equity',
+    'gordon': 'gordon-corporation',
+    'mccullough': 'mccullough-mechanical'
+}
 
 
 @bot.command(name='build-facility', help='Builds a facility')
 @commands.has_role('control')
-async def play_card(ctx: commands.context.Context, corporation_name: str, facility_type: str, facility_name: str):
-    if corporation_name not in CORPORATION_NAMES:
+async def build_facility(ctx: commands.context.Context, short_corp: str, facility_type: str, facility_name: str):
+    if short_corp not in CORPORATION_NAMES:
         await ctx.send(
-            f'{0} not found (must be one of {1})'.format(
-                corporation_name,
-                ','.join(str(x) for x in CORPORATION_NAMES)
+            '{0} not found (must be one of {1})'.format(
+                short_corp,
+                ', '.join(CORPORATION_NAMES)
             )
         )
-        return
-
-    if facility_type not in FACILITY_TYPES:
-        await ctx.send(f'Unknown facility type {facility_type}')
         return
 
     guild = ctx.guild
@@ -213,14 +247,14 @@ async def play_card(ctx: commands.context.Context, corporation_name: str, facili
 
     assert channel is not None, "facility-list was not found?"
 
-    corporation_name = CORPORATION_NAMES[corporation_name]
+    corporation_name = CORPORATION_NAMES[short_corp]
 
     facilities = []
     message_to_edit: discord.message = None
 
     async for message in channel.history():
         if corporation_name in message.content:
-            facilities = [[y.strip() for y in x.split('|')[1:-1]] for x in message.content.split("\n")[4:-1]]
+            facilities = await facility_from_message(message)
             message_to_edit = message
             break
 
@@ -232,13 +266,82 @@ async def play_card(ctx: commands.context.Context, corporation_name: str, facili
     facilities.append([facility_name, facility_type])
 
     from tabulate import tabulate
-    message_contents = f'{corporation_name} facilities:\n```\n{tabulate(facilities, ["Facility name", "Facility Type"], tablefmt="github")}\n```'
+    table_string = tabulate(facilities, ["Facility name", "Facility Type"], tablefmt="github")
+    message_contents = f'{corporation_name} facilities:\n```\n{table_string}\n```'
 
     await channel.send(message_contents)
 
     if message_to_edit:
         await message_to_edit.delete()
 
+    # Now create a run channel
+    channel_name = f'{short_corp}-{facility_name.lower()}'
+    control = discord.utils.get(guild.roles, name='control')
+
+    role = discord.utils.get(guild.roles, name=CORPORATION_ROLE_NAMES[short_corp])
+    category = discord.utils.get(guild.categories, name='runs')
+
+    await guild.create_text_channel(
+        name=channel_name,
+        category=category,
+        overwrites={
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            control: discord.PermissionOverwrite(read_messages=True),
+            role: discord.PermissionOverwrite(read_messages=True)
+        }
+    )
+
+    await guild.create_voice_channel(
+        name=channel_name,
+        category=category,
+        overwrites={
+            guild.default_role: discord.PermissionOverwrite(view_channel=False),
+            control: discord.PermissionOverwrite(view_channel=True),
+            role: discord.PermissionOverwrite(view_channel=True)
+        }
+    )
+
     await ctx.send(f'{ctx.message.author.mention} - facility built')
+
+
+@bot.command(name='remove-facility', help='Remove a facility')
+@commands.has_role('control')
+async def remove_facility(ctx: commands.context.Context, short_corp: str, facility_name: str):
+    from discord import TextChannel
+    from tabulate import tabulate
+
+    guild = ctx.guild
+
+    channel: TextChannel = discord.utils.get(guild.channels, name='facility-list')
+
+    assert channel is not None, "facility-list was not found?"
+
+    corporation_name = CORPORATION_NAMES[short_corp]
+
+    async for message in channel.history():
+        if corporation_name in message.content:
+            facilities = await facility_from_message(message)
+            facilities = list(filter(lambda x: x[0] != facility_name, facilities))
+
+            table_string = tabulate(facilities, ["Facility name", "Facility Type"], tablefmt="github")
+            message_contents = f'{corporation_name} facilities:\n```\n{table_string}\n```'
+
+            await channel.send(message_contents)
+            await message.delete()
+
+            channel = discord.utils.get(guild.channels, name=f'{short_corp}-{facility_name.lower()}')
+
+            while channel:
+                await channel.delete()
+                channel = discord.utils.get(guild.channels, name=f'{short_corp}-{facility_name.lower()}')
+
+            await ctx.send(f'{ctx.message.author.mention} - facility removed')
+
+            break
+
+
+async def facility_from_message(message):
+    return [[y.strip() for y in x.split('|')[1:-1]] for x in message.content.split("\n")[4:-1]]
+
 
 bot.run(TOKEN)
